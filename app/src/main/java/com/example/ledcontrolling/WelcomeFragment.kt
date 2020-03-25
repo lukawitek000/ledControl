1fad3aedfc08c518f89940486ce180af615b4154
Łukasz <lukasz.witkowski000@gmail.com>
2020-03-25 17:23:57 +0100

    data bind welcome fragment

--- app/src/main/java/com/example/ledcontrolling/WelcomeFragment.kt
+++ app/src/main/java/com/example/ledcontrolling/WelcomeFragment.kt
@@ -5,23 +5,34 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import com.example.ledcontrolling.databinding.WelcomeFragmentBinding
 
 class WelcomeFragment : Fragment(){
+
+    private lateinit var binding: WelcomeFragmentBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.welcome_fragment, container, false)
-        val button = view.findViewById<Button>(R.id.button)
+        binding = DataBindingUtil.inflate(inflater, R.layout.welcome_fragment, container, false)
 
-        button.setOnClickListener{
-            view.findNavController().navigate(R.id.action_welcomeFragment_to_colorPickerFragment)
+        //val view = inflater.inflate(R.layout.welcome_fragment, container, false)
+        //val button = view.findViewById<Button>(R.id.button)
+       // val button = binding.button
+
+
+
+        binding.button.setOnClickListener{
+            binding.root.findNavController().navigate(R.id.action_welcomeFragment_to_colorPickerFragment)
+
         }
 
 
-        return view
+        return binding.root
     }
 }
